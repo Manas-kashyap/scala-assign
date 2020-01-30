81690487db7b7514554b62f8956dc3dd5e245eab
Manas-Kashyap <manaskashyaptech@gmail.com>
2020-01-30 22:15:03 +0530

    Linear and Binary search done

--- src/main/scala/edu/knoldus/Searching.scala
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -3,13 +3,25 @@ package edu.knoldus
 class Searching {
 
   def binarySearch(array: Array[Int], elem: Int): Boolean = {
-    //todo: Add Logic
+    var left = 0
+    var right = array.length-1
+    while (left<=right) {
+      val mid = left + (right-left)/2
+      if (array(mid)==elem)
+        {true}
+      else if (array(mid)>elem)
+        {right = mid-1}
+      else
+        {left = mid + 1}
+    }
     true
   }
 
   def linearSearch(array: Array[Int], elem: Int): Boolean = {
-    //todo: Add Logic
-    true
+    if (array.isEmpty) { false }
+    else if (array.head == elem) { true }
+    else { linearSearch(array.tail, elem) }
+      true
   }
 
 
