d53d27886d58b300c7b0eb1cc358c05d6e67b975
Manas-Kashyap <manaskashyaptech@gmail.com>
2020-01-31 10:12:25 +0530

    searching done without var

--- src/main/scala/edu/knoldus/Searching.scala
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -3,18 +3,23 @@ package edu.knoldus
 class Searching {
 
   def binarySearch(array: Array[Int], elem: Int): Boolean = {
-    var left = 0
-    var right = array.length-1
-    while (left<=right) {
-      val mid = left + (right-left)/2
-      if (array(mid)==elem)
-        {true}
-      else if (array(mid)>elem)
-        {right = mid-1}
-      else
-        {left = mid + 1}
-    }
-    true
+    val SortedArray = array.sortWith(_ < _)
+    val k=mySearch(SortedArray,0,array.length-1,elem)
+    k
+  }
+  def mySearch(array:Array[Int],l:Int,r:Int,elem:Int):Boolean= {
+    if (r >= l) {
+      val mid = l + (r - l) / 2
+      if (array(mid) == elem)
+      {
+        return true
+      }
+      if (array(mid) > elem) {
+        mySearch(array, l, mid - 1, elem)
+      }
+      mySearch(array, mid + 1, r, elem)
+    }else{
+    false }
   }
 
   def linearSearch(array: Array[Int], elem: Int): Boolean = {
